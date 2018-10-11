9f2d25c1a6838011e41390ef06bef4c2dce242e8
ehteshamkaushik <ehteshamkaushik@gmail.com>
2018-10-12 04:28:42 +0600

    Basic app without camera

--- app/src/main/java/com/example/kaushik/imagedatacollection/MainActivity.java
+++ app/src/main/java/com/example/kaushik/imagedatacollection/MainActivity.java
@@ -4,34 +4,62 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public int backButtonCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText nameEntry = findViewById(R.id.nameEntryId);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        final EditText nameEntry = findViewById(R.id.nameEntryId);
         Button nameSubmit = findViewById(R.id.nameSubmit);
-
-        final String userName = nameEntry.getText().toString();
 
         nameSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String userName = nameEntry.getText().toString();
                 if (userName.isEmpty())
                 {
                     Toast.makeText(MainActivity.this, "Please Enter a Valid Name", Toast.LENGTH_SHORT).show();
                 }
                 else
                 {
-                    
+                    Intent intent = new Intent(MainActivity.this, PostActivity.class);
+                    intent.putExtra("userName", userName);
+                    startActivity(intent);
                 }
             }
         });
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        if(backButtonCount >= 1)
+        {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+        else
+        {
+            Toast.makeText(this, "Press the back button once again to close the application.", Toast.LENGTH_SHORT).show();
+            backButtonCount++;
+        }
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        android.os.Process.killProcess(android.os.Process.myPid());
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/example/kaushik/imagedatacollection/PostActivity.java
+++ app/src/main/java/com/example/kaushik/imagedatacollection/PostActivity.java
@@ -1,7 +1,25 @@
 package com.example.kaushik.imagedatacollection;
 
+import android.app.LauncherActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -9,5 +27,122 @@ public class PostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+        TextView userNameTextView = findViewById(R.id.userNameTextViewId);
+        final TextView postNoTextView = findViewById(R.id.imageNoTextViewId);
+        final Button nextButton = findViewById(R.id.nextButtonId);
+        final ImageView postImageView = findViewById(R.id.postImageViewId);
+        final ImageButton isLikedImageButton = findViewById(R.id.isLikedImageButtonId);
+        final TextView isLikedTextView = findViewById(R.id.isLikedTextViewId);
+        final EditText commentEditText = findViewById(R.id.commentEditTextId);
+        Button commentButton = findViewById(R.id.commentButtonId);
+        final ListView commentListView = findViewById(R.id.commentListViewId);
+        final int[] postNo = {1};
+        final boolean[] isLiked = {false};
+        final List<String> comments = new ArrayList<>();
+
+        final ArrayAdapter<String> commentListAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, comments);
+        commentListView.setAdapter(commentListAdapter);
+
+        final Intent intent = getIntent();
+        final String userName = intent.getExtras().getString("userName");
+        userNameTextView.setText(userName);
+        initializePost(postNo[0], postNoTextView, postImageView, isLikedTextView);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (postNo[0] == 14)
+                {
+                    nextButton.setText("Finish");
+                }
+                if (postNo[0] >= 15)
+                {
+                    Intent intent1 = new Intent(PostActivity.this, MainActivity.class);
+                    startActivity(intent1);
+                }
+                else
+                {
+                    postNo[0]++;
+                    initializePost(postNo[0], postNoTextView, postImageView, isLikedTextView);
+                    isLikedImageButton.setImageResource(R.drawable.like_unpessed);
+                    comments.clear();
+                    commentListAdapter.notifyDataSetChanged();
+                    setListViewHeightBasedOnChildren(commentListView);
+                    isLiked[0] = false;
+                }
+            }
+        });
+
+        isLikedImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isLiked[0])
+                {
+                    isLiked[0] = false;
+                    isLikedImageButton.setImageResource(R.drawable.like_unpessed);
+                    isLikedTextView.setText("You have not liked this post yet");
+                }
+                else
+                {
+                    isLiked[0] = true;
+                    isLikedImageButton.setImageResource(R.drawable.like_pressed);
+                    isLikedTextView.setText("You have liked this post");
+                }
+            }
+        });
+
+        commentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String comment = commentEditText.getText().toString();
+                if (!comment.isEmpty()) {
+                    comments.add(comment);
+                    commentListAdapter.notifyDataSetChanged();
+                    setListViewHeightBasedOnChildren(commentListView);
+                    commentEditText.setText("");
+                    InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                }
+            }
+        });
+    }
+
+    public void initializePost(int postNo, TextView postNoTextView, ImageView postImageView,
+                                      TextView isLikedTextView)
+    {
+        postNoTextView.setText("Post " + postNo);
+        final int postId = PostActivity.this.getResources().getIdentifier("post"+ postNo, "drawable", getPackageName());
+        postImageView.setImageResource(postId);
+        isLikedTextView.setText("You have not liked this post yet");
+    }
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(PostActivity.this, "Please finish the survey", Toast.LENGTH_LONG).show();
+        moveTaskToBack(false);
+    }
+
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = 0;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
     }
 }
